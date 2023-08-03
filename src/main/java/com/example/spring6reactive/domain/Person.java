9f627ea22599f9c609704d2b34c42c7a91b41c0d
ibanezo <martin.sterjoski@gmail.com>
2023-08-03 09:57:06 +0200

    Person and PersonRepository added.

---
+++ src/main/java/com/example/spring6reactive/domain/Person.java
@@ -0,0 +1,18 @@
+package com.example.spring6reactive.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+}
---
+++ src/test/java/com/example/spring6reactive/repositories/PersonRepositoryImplTest.java
@@ -0,0 +1,113 @@
+package com.example.spring6reactive.repositories;
+
+import com.example.spring6reactive.domain.Person;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonRepositoryImplTest {
+
+    PersonRepository personRepository = new PersonRepositoryImpl();
+
+    @Test
+    void testMonoByIdBlock() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        Person person = personMono.block();
+
+        System.out.println(person.toString());
+    }
+
+    @Test
+    void testGetByIdSubscriber() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        // prefered with subscribe, no blocking
+        personMono.subscribe(person -> {
+            System.out.println(person.toString());
+        });
+    }
+
+    @Test
+    void testMapOperation() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        personMono.map(person -> person.getFirstName())
+                .subscribe(firstname -> {
+                    System.out.println(firstname);
+                });
+    }
+
+    @Test
+    void testFluxBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Person person = personFlux.blockFirst();
+
+        System.out.println(person.toString());
+    }
+
+    @Test
+    void testFluxSubscriber() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.subscribe(person1 -> {
+            System.out.println(person1.toString());
+        });
+    }
+
+    @Test
+    void testFluxMap() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.map(Person::getFirstName).subscribe(firstName -> System.out.println(firstName));
+    }
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = personRepository.findAll();
+        Mono<List<Person>> listMono = personFlux.collectList();
+
+        listMono.subscribe(list -> {
+            list.forEach(person -> System.out.println(person.getFirstName()));
+        });
+    }
+
+    @Test
+    void testFilterOnName() {
+        personRepository.findAll()
+                .filter(person -> person.getFirstName().equals("Fiona"))
+                .subscribe(person -> System.out.println(person.getFirstName()));
+    }
+
+    @Test
+    void testGetById() {
+        Mono<Person> fionaMono = personRepository.findAll().filter(person -> person.getFirstName().equals("Fiona")).next();
+
+        fionaMono.subscribe(person -> System.out.println(person.getFirstName()));
+    }
+
+    @Test
+    void testFindPersonByIdNotFound() {
+        Flux<Person> personFlux = personRepository.findAll();
+        final Integer id = 8;
+
+        Mono<Person> personMono = personFlux.filter(person -> person.getId() == id).single()
+                .doOnError(throwable -> {
+                    System.out.println("Error occurred in the flux");
+                    System.out.println(throwable.toString());
+                });
+
+        // we need backpressure so the reactive be executed
+        personMono.subscribe(person -> {
+            System.out.println(person.toString());
+        }, throwable -> {
+            System.out.println("Error occurred in the mono");
+            System.out.println(throwable.toString());
+        });
+    }
+}
